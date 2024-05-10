c4ebc864b5a1bb1087146d9e340d0d2fa63e76b0
Andrea Tagliazucchi <andreatagliazucchi@gmail.com>
2024-05-10 11:37:56 +0200

    update

---
+++ src/assets/_scriptserver/scriptserver_modificato.py
@@ -0,0 +1,77 @@
+import requests, datetime
+key = 'BlQgytxF5EBpU5KK7W13c4a742a3vpdtrGuvh3xA' # r/w all
+
+h = {
+    'Accept': 'application/json',
+    'Authorization': f'Bearer {key}'
+}
+
+order = {
+    DA MODIFICARE
+    'order_items'.  sale_items : [
+        {
+            'item_id': 954,
+            'name': 'Nigiri Special',
+            'uuid': '88a11135-f7e1-4f12-b5b0-35715e30b945',
+            'net_price': 12.73,
+            'vat_perc': 10,
+            'final_price': 28,
+            'final_net_price': 25.46,
+            'notes': '',
+            'price': 14,
+            'quantity': 2,
+            'category_id': 37,
+            'category_name': 'Sushi Signatures',
+
+
+            DA MODIFICARE:
+            'operator_id': 0,   (seller_id)
+            'operator_name': 'Ordini da Web' (seller_name)
+
+
+            DA AGGIUNGERE
+            'type': "sale",
+            "exit": "1",  
+
+
+            DA AGGIUNGERE - FORSE BASTA NON METTERLE
+            "department_id": "3",  
+            "department_name": "VAT 10% - Services",  
+            
+
+        }
+    ],
+
+    DA MODIFICARE
+    'order_customer' (sale_customer): {
+        'email': 'renato@junto.space',
+        'first_name': 'Renato',
+        'last_name': 'Fabbri',
+        'mobile': '3515685557'
+    },
+
+    DA MODIFICARE
+    'type': 'take_away',  (order_type: 'take-awey')
+
+    'status': 'open',
+    'name': 'EXAMPLE - Renato Fabbri',
+
+    DA MODIFICARE
+    'operator_id': 0,   (seller_id)
+    'operator_name': 'Ordini da Web' (seller_name)
+
+    DA AGGIUNGERE
+    "auto_print_order": true,
+    "currency": "EUR",
+
+
+
+    'open_at': '2023-04-27T18:49:56.035080',
+    'deliver_at': '2023-04-27T18:45:00.000000Z'
+}
+
+resp = requests.post(
+    'https://api.tilby.com/v2/orders',
+    json=order,
+    headers=h,
+)
